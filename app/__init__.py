e11f49fd7f583c70e40e89df0ab79d38087a9a8f
lsnn f <lsnn f>
2016-03-15 21:13:48 +0800

    程序基本框架搭建完成

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLALchemy
+
 from config import config
 
 bootstrap = Bootstrap()
@@ -19,5 +20,8 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
